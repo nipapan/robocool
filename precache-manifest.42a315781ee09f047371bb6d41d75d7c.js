3436a45a7c0e489a1d7e960bd9d13624e84cae49
Nipapan Aom <nipapan@maqe.com>
2019-11-22 18:38:09 +0700

    Updates

---
+++ precache-manifest.42a315781ee09f047371bb6d41d75d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82f50f64b1a0610b74a5cbad6e39423",
+    "url": "/robocool/index.html"
+  },
+  {
+    "revision": "770e12122a82ed0e10d9",
+    "url": "/robocool/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6897983143f2346be066",
+    "url": "/robocool/static/css/main.13a77a4e.chunk.css"
+  },
+  {
+    "revision": "770e12122a82ed0e10d9",
+    "url": "/robocool/static/js/2.6aa0823c.chunk.js"
+  },
+  {
+    "revision": "6897983143f2346be066",
+    "url": "/robocool/static/js/main.11108ec7.chunk.js"
+  },
+  {
+    "revision": "2c37ed9e5d44bcb17794",
+    "url": "/robocool/static/js/runtime-main.14468c02.js"
+  }
+]);
